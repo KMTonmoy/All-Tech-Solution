95eb1e83a1e200070339bbce0ddd5dc8346acbe9
TONMOY <tonmoyahamed2009@gmail.com>
2023-12-23 10:55:00 +0600

    Compleate All Tech Solution

---
+++ App.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#element', {
+    strings: ['Frontend Developer', 'Backend Developer', "Web Developer", 'Freelancer'],
+    typeSpeed: 60,
+    backSpeed:100,
+    loop:true,
+  });
+
+var typed = new Typed('#hel', {
+    strings: ["Developer", 'Designer'],
+    typeSpeed: 100,
+    backSpeed:200,
+    loop:true,
+  });
+
+
